baf27afd11e8932c850f55748d86f0850dc59a10
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2019-06-21 15:54:56 -0300

    teste

--- rsa_encrypt.cpp
+++ rsa_encrypt.cpp
@@ -41,7 +41,7 @@ int find_e(int d, int z)
 
 }
 
-int prime_each_other(int d, int z)
+int is_prime_each_other(int d, int z)
 {
   if(is_prime_number(d))
     return 0;
@@ -56,16 +56,52 @@ int prime_each_other(int d, int z)
 //o ideal é separar a informacoes em 4bits, pq esse é o limite la q o professor disse q vai tirar D quem nao fizer
 //operar os bits de forma a criptografar cada 4 bits
 
+void encrypt(char *input, int p, int q, int d)
+{
+
+  int size = sizeof(input);
+  int i, j, n, z, e;
+  unsigned int c;
+
+  if(!is_prime_number(p) || !is_prime_number(q)){
+    cout << "P ou Q não é primo!" << endl;
+    return;
+  }
+
+  z = (p - 1) * (q - 1);
+
+  if(is_prime_each_other(d, z)){
+    cout << "Não é primo entre si!" << endl;
+    return;
+  }
+
+  n = p * q;
+  e = find_e(d, z);
+
+  for(i = 0; i < size; i++){
+
+      c = 0x00;
+      c = input[i] & 0xF;
+      c = fmod(pow(c, e), n);
+      input[i] = input[i] | c;
+
+      c = 0x00;
+      c = input[i] & 0xF0;
+      c = c >> 4;
+      c = fmod(pow(c, e), n);
+      input[i] = input[i] | (c << 4);
+
+  }
+
+}
+
 int main()
 {
 
-  int n = 234223243;
+  char c[] = "eduar";
 
-  if(is_prime_number(n)){
-    cout << "É primo!" << endl;
-  }else{
-    cout << "Não é primo" << endl;
-  }
+  encrypt(c, 3, 11, 7);
+  cout << "Teste: " << c << endl;
 
   return 0;
 }
